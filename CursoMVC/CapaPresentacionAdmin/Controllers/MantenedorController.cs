354b6df0a755a252eaba35cfb888ed68bb994221
Winnie Solis <winniesolis97@gmail.com>
2023-10-18 16:13:20 -0600

    Parcialidad de Categorias

--- CursoMVC/CapaPresentacionAdmin/Controllers/MantenedorController.cs
+++ CursoMVC/CapaPresentacionAdmin/Controllers/MantenedorController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using CapaEntidad;
+using CapaNegocio;
+
 namespace CapaPresentacionAdmin.Controllers
 {
     public class MantenedorController : Controller
@@ -13,6 +16,57 @@ namespace CapaPresentacionAdmin.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public JsonResult ListarCategorias()
+        {
+            List<Categoria> oLista = new List<Categoria>();
+
+            oLista = new CN_Categoria().Listar();
+
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public JsonResult GuardarCategorias(Categoria objeto)
+        {
+            object resultado;
+            string mensaje = string.Empty;
+
+            if (objeto.IdCategoria == 0)
+            {
+                resultado = new CN_Categoria().Registrar(objeto, out mensaje);
+            }
+            else
+            {
+                resultado = new CN_Categoria().Editar(objeto, out mensaje);
+            }
+
+            return Json(new { resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public JsonResult EliminarUsuario(int id)
+        {
+
+            bool respuesta = false;
+            string mensaje = string.Empty;
+
+            respuesta = new CN_Categoria().Eliminar(id, out mensaje);
+
+            return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+
+
+
+
+
+
+
         public ActionResult Marca()
         {
             return View();
@@ -21,5 +75,11 @@ namespace CapaPresentacionAdmin.Controllers
         {
             return View();
         }
+
+
+
+
+
+
     }
 }
